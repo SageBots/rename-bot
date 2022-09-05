69e066dedd7b6dfa13daff95f96b55b449706b9c
SageBots <112871122+SageBots@users.noreply.github.com>
2022-09-05 19:33:47 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -2,19 +2,19 @@ from pyrogram import Client, filters
 from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
 import humanize
 from helper.database import  insert 
-SUPPORT_CH = 'Crazebots'
-YOUTUBE = 'TechnologyRk'
+SUPPORT_CH = 'SageBots'
+
 @Client.on_message(filters.private & filters.command(["start"]))
 async def start(client,message):
 	insert(int(message.chat.id))
 	await message.reply_text(text =f"""
 	Hello {message.from_user.first_name }
 	__I am file renamer bot, Please sent any telegram 
-	**Document Or Video** and enter new filenameto rename it__
+	**Document Or Video** and enter new filename to rename it__
 	""",reply_to_message_id = message.message_id ,  
 	reply_markup=InlineKeyboardMarkup(
-	 [[ InlineKeyboardButton("Support 🇮🇳" ,url=f"https://t.me/{SUPPORT_CH}") ], 
-	[InlineKeyboardButton("Subscribe 🧐", url=f"https://youtube.com/c/{YOUTUBE}") ]  ]))
+	 [[ InlineKeyboardButton("Support 🇮🇳" ,url=f"https://t.me/{SUPPORT_CH}") ]
+	 ]
 
 
 @Client.on_message(filters.private &( filters.document | filters.audio | filters.video ))
